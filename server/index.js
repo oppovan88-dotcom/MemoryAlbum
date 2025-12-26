cc145d992094a993c23fd1f7c8fd3213cb1a265b
AceZero37 <gojoling6@gmail.com>
2025-12-26 16:07:47 +0700

    Fix 413 error - increase body size limit for image uploads

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
